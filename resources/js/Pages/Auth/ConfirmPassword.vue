dd918a74b3f39ce7c55aa8abe108089c7349ee5a
FilipJ10 <121499248+FilipJ10@users.noreply.github.com>
2023-01-13 15:34:58 +0100

    ConfirmPassword updates

--- resources/js/Pages/Auth/ConfirmPassword.vue
+++ resources/js/Pages/Auth/ConfirmPassword.vue
@@ -22,7 +22,7 @@ const submit = () => {
         <Head title="Confirm Password" />
 
         <div class="mb-4 text-sm text-gray-600">
-            This is a secure area of the application. Please confirm your password before continuing.
+            Ovo je bezbjedna oblast aplikacije. Molimo Vas da potvrdite svoju lozinku prije nego što nastavite.
         </div>
 
         <form @submit.prevent="submit">
